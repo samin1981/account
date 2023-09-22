9f380a2d28788169134fc488d3dd95ddfb4d1bd4
samin <samin.mohammadi81@gmail.com>
2023-09-22 12:33:33 +0330

    implementing restful crud services

--- account/src/main/java/com/example/account/controller/UserController.java
+++ account/src/main/java/com/example/account/controller/UserController.java
@@ -17,29 +17,44 @@ public class UserController {
     @Autowired
     private UsersService usersService;
 
-    //curl http://localhost:9091/api/users
-    @GetMapping("/users")
-    public List<User> list() {
+    //curl http://localhost:9091/api/getAllUsers
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers() {
         return usersService.listAll();
     }
 
-    //curl http://localhost:9091/api/users/1
-    @GetMapping("/users/{id}")
-    public ResponseEntity<User> get(@PathVariable Integer id) {
+    //curl http://localhost:9091/api/getUser/1
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Integer id) {
         try {
             User user = usersService.get(id);
             return new ResponseEntity<User>(user, HttpStatus.OK);
-        }
-       catch (NoSuchElementException ex) {
+        } catch (NoSuchElementException ex) {
             return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
-       }
+        }
     }
 
-    //curl -X POST -H "Content-Type: application/json" -d "{\"name\":\"beti\",\"sex\":0}" http://localhost:9091/api/addUser
+    //curl -X POST -H "Content-Type: application/json" -d "{\"name\":\"samin\",\"sex\":0}" http://localhost:9091/api/addUser
     @PostMapping("/addUser")
     public void addUser(@RequestBody User user) {
         usersService.save(user);
     }
 
-//https://www.codejava.net/frameworks/spring-boot/spring-boot-restful-crud-api-examples-with-mysql-database
+    //curl -X PUT -H "Content-Type: application/json" -d "{\"id\":2,\"name\":\"shana\",\"sex\":0}" http://localhost:9091/api/updateUser/1
+    @PutMapping("/updateUser/{id}")
+    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable Integer id) {
+        try {
+            User existUser = usersService.get(id);
+            usersService.save(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (NoSuchElementException ex) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //curl -X DELETE http://localhost:9091/api/deleteUser/5
+    @DeleteMapping("deleteUser/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        usersService.delete(id);
+    }
 }
