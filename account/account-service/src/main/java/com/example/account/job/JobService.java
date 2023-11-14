376609e4693f466a6baf6a8ff765b1f9bd6aa40a
samin <samin.mohammadi81@gmail.com>
2023-11-14 20:29:34 +0330

    sprint 4

--- account/account-service/src/main/java/com/example/account/job/JobService.java
+++ account/account-service/src/main/java/com/example/account/job/JobService.java
@@ -1,7 +1,8 @@
 package com.example.account.job;
 
-import com.example.account.service.FacilityService;
 import com.example.account.service.PersonService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -11,6 +12,7 @@ import java.util.Date;
 
 @Service
 public class JobService {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     private final PersonService personService;
     public JobService(PersonService personService) {
 
@@ -19,8 +21,8 @@ public class JobService {
     @Scheduled(cron = "${scheduling.debt.peyment.duedate.job.cron}")
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void debtPaymentDuedate() throws InterruptedException {
-        System.out.println("----------start debtPaymentDuedate job---------------");
+        logger.info("----------Start debt payment duedate job---------------");
         personService.getDebtors(new Date());
-        System.out.println("----------  end debtPaymentDuedate job---------------");
+        logger.info("----------  End debt payment duedate job---------------");
     }
 }
